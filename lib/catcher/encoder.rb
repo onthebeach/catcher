5f0614e2e7e97e8e644836c7571f0276854dc7d1
Charles J Hardy <chuckjhardy@gmail.com>
2013-05-08 14:14:20 +0100

    Handle encoding of unusual characters

--- lib/catcher/encoder.rb
+++ lib/catcher/encoder.rb
@@ -1,7 +1,7 @@
 class Encoder
   def self.encode(string)
     if string.respond_to?(:encode!)
-      string.encode!('UTF-8')
+      string.encode!('UTF-8', :invalid => :replace, :undef => :replace, :replace => '')
     else
       require 'iconv'
       ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', string + ' ')[0..-2]
