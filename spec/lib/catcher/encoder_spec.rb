27a4b2adeb924bd13797aeeaa2909f6bcd551b3b
Davide Dippolito <davide.dippolito@onthebeach.co.uk>
2013-11-15 11:25:11 +0000

    Update encoder to deal with different inputs
    Force usage of iconv for not utf-8 strings.
    Also update travis to include ruby 1.8.7

--- spec/lib/catcher/encoder_spec.rb
+++ spec/lib/catcher/encoder_spec.rb
@@ -1,11 +1,32 @@
+#-*- coding: utf-8 -*-
+
 require 'spec_helper'
+require 'json'
 
 describe Encoder do
+  subject { described_class.encode(string)  }
 
-  let(:encoder) { Encoder.encode(string)  }
-  let(:string) { 'test' }
+  let(:original) { { name: name }.to_json }
+  let(:name) { 'äåéöÄÅÉÖabcdef123098%^&*()%$£"!¿./?_`¬¿ ¿' }
+  let(:string) { original }
 
   it 'returns a string' do
-    expect(encoder).to eq string
+    expect(subject).to be_an_instance_of(String)
+  end
+
+  context 'when passed a UTF8 string' do
+    let(:string) { original.dup.force_encoding('UTF-8') }
+
+    it 'returns given string' do
+      expect(subject).to eq(original)
+    end
+  end
+
+  context 'when passed an ASCII string' do
+    let(:string) { original.dup.force_encoding('ASCII') }
+
+    it 'returns a UTF8 string' do
+      expect(subject).to eq(original)
+    end
   end
 end
