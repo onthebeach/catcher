d2d0562a47495d526d6811556e27b36dd29e3eb4
Davide Dippolito <davide.dippolito@onthebeach.co.uk>
2013-11-18 12:03:06 +0000

    Update encoder to deal with different inputs
    String#encode is not executed if the string has the same encoding of the
    argument, even if it contains invalid chars. In those occasions the iconv
    library was used to force the conversion anyway.
    
    This remove the deprecated (removed) iconv library and use only the standard
    ruby library method "force_encoding" method which does force the
    conversion anyway.

--- lib/catcher/encoder.rb
+++ lib/catcher/encoder.rb
@@ -1,37 +1,5 @@
 class Encoder
-  def initialize(string)
-    @string = string
-  end
-
   def self.encode(string)
-    new(string).encode
-  end
-
-  def encode
-    native? ? native : iconv
-  end
-
-  private
-  attr_reader :string
-
-  def native?
-    encodable? && !ascii?
-  end
-
-  def ascii?
-    string.encoding.to_s.include?('ASCII')
-  end
-
-  def encodable?
-    string.respond_to?(:encode!)
-  end
-
-  def native
-    string.encode!('UTF-8', :invalid => :replace, :undef => :replace, :replace => '')
-  end
-
-  def iconv
-    require 'iconv'
-    ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', string + ' ')[0..-2]
+    string.force_encoding('UTF-8')
   end
 end
