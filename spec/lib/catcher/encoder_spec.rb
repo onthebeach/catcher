c5e410e526bc93912d547bc581b67d292d333230
seenmyfate <seenmyfate@gmail.com>
2014-01-08 14:28:31 +0000

    Better describe encoding behaviour in the test
    In order to avoid any confusion over the behaviour of the encoder, this
    test better describes what's happening.

--- spec/lib/catcher/encoder_spec.rb
+++ spec/lib/catcher/encoder_spec.rb
@@ -1,11 +1,13 @@
+#-*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Encoder do
 
-  let(:encoder) { Encoder.encode(string)  }
-  let(:string) { 'test' }
+  let(:encoder) { Encoder.encode(ascii_string)  }
+  let(:ascii_string) { 'äåéöÄÅÉÖabcdef123098%^&*()%$£"!¿./?_`¬¿ ¿'.force_encoding('ASCII')  }
+  let(:utf8_string) { 'abcdef123098%^&*()%$"!./?_` ' }
 
   it 'returns a string' do
-    expect(encoder).to eq string
+    expect(encoder).to eq utf8_string
   end
 end
