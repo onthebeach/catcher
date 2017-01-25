93dbf1e12e0173793ac9efa512914835ad61c688
Gavin Laking <gavinlaking@gmail.com>
2017-01-25 13:51:44 +0000

    Revert "Fix handing of unusual characters"
    This reverts commit 405d6f65b7573a182f0bf1f36e3ee9ca9b507561.

--- lib/catcher/encoder.rb
+++ lib/catcher/encoder.rb
@@ -1,7 +1,7 @@
 class Encoder
   def self.encode(string)
     if string.respond_to?(:encode!)
-      string.encode!('UTF-8', :invalid => :replace, :undef => :replace, :replace => '')
+      string.encode!('UTF-8')
     else
       require 'iconv'
       ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', string + ' ')[0..-2]
